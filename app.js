e269fa9e76b7ee157b887e380628c14c2fa453fb
kushagrasingh2306 <9a.kushagrasingh@gmail.com>
2026-09-20 20:36:52 +0530

    GET /assignments

--- app.js
+++ app.js
@@ -25,6 +25,22 @@ app.post('/assignments', async (req,res) => {
     }
 });
 
+app.get('/assignments', async (req, res) => {
+    try{
+        let result = await pool.query(
+            `SELECT * FROM assignments
+            ORDER BY id DESC;`
+        );
+        res.status(200).json(result.rows);
+    }catch(err){
+        console.log(err.message);
+        res.status(500).json({
+            errorMessage: 'Server is Down'
+        });
+    }
+});
+
+
 
 
 app.listen(PORT, () => {
