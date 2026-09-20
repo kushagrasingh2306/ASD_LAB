52eb32329aae1522b684d930aec8c868d57d81a1
kushagrasingh2306 <9a.kushagrasingh@gmail.com>
2026-09-20 20:40:41 +0530

    DELETE /assignments/:id

--- app.js
+++ app.js
@@ -64,6 +64,28 @@ app.patch('/assignments/:id', async(req, res) => {
     }
 });
 
+app.delete('/assignments/:id', async(req, res) => {
+    try{
+        const { id } = req.params;
+        let result = await pool.query(
+            `DELETE FROM assignments
+                WHERE id = $1
+                 RETURNING *;`,
+            [id]
+        );
+        if(result.rows.length === 0){
+            return res.status(404).json({
+                errorMessage: 'Assignments Not Found'
+            });
+        }
+        res.status(200).json(result.rows[0])
+    }catch(err){
+        console.log(err.message)
+        res.status(500).json({
+            errorMessage: 'Server is closed'
+        });
+    }
+});
 
 
 app.listen(PORT, () => {
